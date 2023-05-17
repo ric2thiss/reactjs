7a16a7417c2327c9d2023ad32a4000d494c7b157
Ric Charles Paquibot <ric2thiss@gmail.com>
2023-05-17 15:47:05 +0800

    test

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
       <div className={darkMode ? "dark":""}>
         <Head>
           <title>Ric Charles Portfolio</title>
-          <meta name="Freelance full-stack web developer expertise(HTML, CSS, ReactJS, PHP, Laravel)" content="Portfolio"/>
+          <meta name="google-site-verification" content="7g7wtSSPdQ_r8TkE81s-7KCIZnzgNTgbP500ZZQFKXM" />
           <link rel="icon" href="/favicon.ico"/>
         </Head>
 
